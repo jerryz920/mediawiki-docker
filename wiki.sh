3f48fc246a62d8da4200fb6b2d953278d23ea37b
Yan Zhai <yanzhai@cs.wisc.edu>
2017-05-06 23:13:22 -0500

    socc2017

--- wiki.sh
+++ wiki.sh
@@ -1,8 +1,8 @@
 
- docker run --rm -p 1080:1080 -p 10443:10443 --name mediawiki \
+ docker run --rm -p 80:80 -p 1080:1080 -p 10443:10443 --hostname=my-wiki --name mediawiki \
    	-e MEDIAWIKI_DB_TYPE=mysql \
-        -e MEDIAWIKI_DB_HOST=10.10.1.46 \
-        -e MEDIAWIKI_DB_PORT=13306 \
+        -e MEDIAWIKI_DB_HOST=10.10.1.36 \
+        -e MEDIAWIKI_DB_PORT=3307 \
         -e MEDIAWIKI_DB_USER=wikiuser \
         -e MEDIAWIKI_DB_PASSWORD=wikipass \
         -e MEDIAWIKI_DB_NAME=wikidb \
